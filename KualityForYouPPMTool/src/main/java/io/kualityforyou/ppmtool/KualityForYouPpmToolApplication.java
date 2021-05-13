c4beecee86ee543cc887a8bc5fb1dc82ce3b1adc
Hardik Jain <hardikjain@Hardiks-MacBook-Air.local>
2021-05-12 22:10:10 -0700

    Initial commit

---
+++ KualityForYouPPMTool/src/main/java/io/kualityforyou/ppmtool/KualityForYouPpmToolApplication.java
@@ -0,0 +1,13 @@
+package io.kualityforyou.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KualityForYouPpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KualityForYouPpmToolApplication.class, args);
+	}
+
+}
---
+++ KualityForYouPPMTool/src/test/java/io/kualityforyou/ppmtool/KualityForYouPpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kualityforyou.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KualityForYouPpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
